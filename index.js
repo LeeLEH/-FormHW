aba0761494949f42bb293cebd10e747b2e2c75d7
LeeLEH <43663799+LeeLEH@users.noreply.github.com>
2020-07-02 19:45:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("index.js input ok");
+
+function changeImage(imageName) {
+  document.getElementById("img").innerHTML =
+    "<img src='img/" + imageName + ".jpg' alt='' width='500' height='600'>";
+}
+
+var localData = [
+  "跑馬古道",
+  "羅東夜市",
+  "窯烤山寨村",
+  "幾米主題廣場",
+  "礁溪鄉五峰旗風景區",
+  "礁溪湯圍溝",
+];
+
+var liContent = "";
+
+for (var i = 0; i < localData.length; i++) {
+  liContent +=
+    "<li><button onclick='changeImage(this.value)' value='" +
+    localData[i] +
+    "'>" +
+    localData[i] +
+    "</button></li>";
+}
+
+document.getElementById("liContent").innerHTML = liContent;
